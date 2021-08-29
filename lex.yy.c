2e284e39e352325ca8841d17c8a3f73d5b95e441
Renan Moura <naneraruom@gmail.com>
2021-08-29 15:09:07 -0300

    operators

--- lex.yy.c
+++ lex.yy.c
@@ -351,8 +351,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 1
-#define YY_END_OF_BUFFER 2
+#define YY_NUM_RULES 6
+#define YY_END_OF_BUFFER 7
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -360,9 +360,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[6] =
+static const flex_int16_t yy_accept[12] =
     {   0,
-        0,    0,    2,    1,    0
+        0,    0,    7,    6,    4,    2,    3,    5,    1,    1,
+        0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -371,8 +372,8 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    2,    3,    1,    4,    1,    5,    6,    6,    6,
+        6,    6,    6,    6,    6,    6,    6,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -397,29 +398,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[2] =
+static const YY_CHAR yy_meta[7] =
     {   0,
-        1
+        1,    1,    1,    1,    1,    2
     } ;
 
-static const flex_int16_t yy_base[7] =
+static const flex_int16_t yy_base[13] =
     {   0,
-        0,    0,    2,    3,    3,    0
+        0,    0,    8,    9,    9,    9,    9,    9,    0,    0,
+        9,    5
     } ;
 
-static const flex_int16_t yy_def[7] =
+static const flex_int16_t yy_def[13] =
     {   0,
-        6,    6,    5,    5,    0,    5
+       11,    1,   11,   11,   11,   11,   11,   11,   12,   12,
+        0,   11
     } ;
 
-static const flex_int16_t yy_nxt[5] =
+static const flex_int16_t yy_nxt[16] =
     {   0,
-        4,    5,    3,    5
+        4,    5,    6,    7,    8,    9,   10,   11,    3,   11,
+       11,   11,   11,   11,   11
     } ;
 
-static const flex_int16_t yy_chk[5] =
+static const flex_int16_t yy_chk[16] =
     {   0,
-        6,    3,    5,    5
+        1,    1,    1,    1,    1,    1,   12,    3,   11,   11,
+       11,   11,   11,   11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,10 +444,10 @@ char *yytext;
 #line 1 "renan-moura.l"
 #line 2 "renan-moura.l"
 //definições em c
-#line 443 "lex.yy.c"
+#line 448 "lex.yy.c"
 #line 6 "renan-moura.l"
     //definições regulares
-#line 446 "lex.yy.c"
+#line 451 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -659,12 +664,10 @@ YY_DECL
 		}
 
 	{
-#line 9 "renan-moura.l"
-
+#line 13 "renan-moura.l"
 
     //tokens
-
-#line 668 "lex.yy.c"
+#line 671 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -691,13 +694,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 6 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 3 );
+		while ( yy_base[yy_current_state] != 9 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -723,10 +726,35 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 13 "renan-moura.l"
+#line 15 "renan-moura.l"
+{printf("NUMERO: %s\n", yytext); }
+	YY_BREAK
+case 2:
+YY_RULE_SETUP
+#line 16 "renan-moura.l"
+{printf("SOMA: %s\n", yytext); }
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 17 "renan-moura.l"
+{printf("SUBTRAÇÃO: %s\n", yytext); }
+	YY_BREAK
+case 4:
+YY_RULE_SETUP
+#line 18 "renan-moura.l"
+{printf("MULTIPLICAÇÃO: %s\n", yytext); }
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 19 "renan-moura.l"
+{printf("DIVISÃO: %s\n", yytext); }
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
+#line 21 "renan-moura.l"
 ECHO;
 	YY_BREAK
-#line 730 "lex.yy.c"
+#line 758 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1023,7 +1051,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 6 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1051,11 +1079,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 6 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 5);
+	yy_is_jam = (yy_current_state == 11);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1731,7 +1759,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 13 "renan-moura.l"
+#line 21 "renan-moura.l"
 
 
 int main(int argc, char **argv){
